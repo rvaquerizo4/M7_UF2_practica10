6190d3b02fc2c5c393738ee830567d7ba7e278a3
Raúl Vaquerizo <113448166+rvaquerizo4@users.noreply.github.com>
2023-01-17 11:24:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import index
 import pandas as pd
 import matplotlib.pyplot as plt
-import PIL
-from PIL import Image
-from PIL import _imaging
+
 
 
 def graficos():
@@ -12,11 +10,11 @@ def graficos():
     filterCiudad = nombres.pop("City")
     filterValores = nombres.pop("Population")
     cast= float(filterValores.replace(',',''))
+    fig1, ax1 = plt.subplots()
     colores = ["#EE6055", "#60D394", "#AAF683", "#FFD97D", "#FF9B85", "#ff33d1", "#33b4ff", "#ff8633", "#33fff3", "#af33ff" ]
-    #plt.pie(cast, labels=filterCiudad, autopct="%0.1f %%", colors=colores)
-    #plt.axis("equal")
-    #plt.show
-    print(cast)
+    ax1.pie(cast, labels=filterCiudad, autopct="%0.1f %%", colors=colores, shadow=True, startangle=90)
+    ax1.axis('equal')
+    plt.show()
 
 graficos()
 
